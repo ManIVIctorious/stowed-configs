56ee4819533206701d3030dba57edf9daeba0ab5
Manuel Jürgen Schuler <manuel.schuler@student.uibk.ac.at>
2021-03-17 19:02:11 +0100

    Nemo Actions: Simplify EbookConverter script

--- nemo/.local/share/nemo/actions/ConvertEbook.sh
+++ nemo/.local/share/nemo/actions/ConvertEbook.sh
@@ -3,18 +3,6 @@
 fileextension="$1"
 shift
 
-for i in "$@"
-do
-    inputfile="$i"
-    filename=${inputfile%.*}
-    ebook-convert  "${inputfile}" "${filename}.${fileextension}"
-
-#echo -e "DEBUG"
-#echo -e "-------------------------------------------------------"
-#echo -e "fileextension   $fileextension"
-#echo -e "inputfile       $inputfile"
-#echo -e "filename        $filename"
-#echo -e "-------------------------------------------------------"
-#echo -e "GUBED"
-
+for inputfile in "$@"; do
+  ebook-convert "${inputfile}" "${inputfile%.*}.${fileextension}"
 done
